210f18c36c90fc96c78dda67e75e320e849b5df2
Drifty-x <gracekkelly74@gmail.com>
2024-08-12 22:00:11 -0400

    Adding base functionality to UI

---
+++ MagicalGirlJamUnity2024/Assets/GameScripts/Menu_UI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_UI : MonoBehaviour
+{
+
+    public void LevelSelect()
+    {
+        
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
